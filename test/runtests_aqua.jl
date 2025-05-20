c530f8a569dafd670dda5bfc7e7ed18529ca92dc
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-05-20 23:00:01 +0200

    ParseUnparse is registered now (#9)

--- test/runtests_aqua.jl
+++ test/runtests_aqua.jl
@@ -3,6 +3,6 @@ module TestAqua
     using Test
     using Aqua: Aqua
     @testset "Aqua.jl" begin
-        Aqua.test_all(ParseUnparseWKTCRS; persistent_tasks = false)
+        Aqua.test_all(ParseUnparseWKTCRS)
     end
 end
